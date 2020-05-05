5e53f86279137442da33b71ca40e1107ce8e7c85
Navigator12 <mykhailo.rusyn@uzhnu.edu.ua>
2020-05-05 20:05:16 +0300

    Add auth pages

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,66 @@
-import React from "react";
-import styles from "./LoginPage.module.scss";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const LoginPage = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [labelClass, setLabelClass] = useState({
+    email: '',
+    password: '',
+  });
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+    if (event.target.value)
+      setLabelClass({ ...labelClass, [event.target.name]: 'active' })
+    else
+      setLabelClass({ ...labelClass, [event.target.name]: '' })
+  }
+
   return (
-    <div>
-      <h2>Login Page</h2>
+    <div className="form">
+      <ul className="tab-group">
+        <li className="tab active"><Link to='/login'>Log In</Link></li>
+        <li className="tab"><Link to='/register'>Sign Up</Link></li>
+      </ul>
+
+      <div className="tab-content">
+        <h1>Welcome Back!</h1>
+        <form>
+          <div className="field-wrap">
+            <label className={labelClass.email}>
+              Email Address<span className="req">*</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              required
+              autoComplete="off"
+              onChange={changeHandler}
+            />
+          </div>
+
+          <div className="field-wrap">
+            <label className={labelClass.password}>
+              Password<span className="req">*</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={form.password}
+              required
+              autoComplete="off"
+              onChange={changeHandler}
+            />
+          </div>
+
+          <input type="button" className="button button-block" value="Log In"/>
+        </form>
+      </div>
     </div>
   );
 };
--- client/src/pages/RegisterPage/RegisterPage.jsx
+++ client/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const RegisterPage = () => {
+  const [form, setForm] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+  });
+
+  const [labelClass, setLabelClass] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    password: '',
+  });
+
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+    if (event.target.value)
+      setLabelClass({ ...labelClass, [event.target.name]: 'active' })
+    else
+      setLabelClass({ ...labelClass, [event.target.name]: '' })
+  }
+
   return (
-    <div>
-      <h2>Register Page</h2>
+    <div className="form">
+      <ul className="tab-group">
+        <li className="tab"><Link to='/login'>Log In</Link></li>
+        <li className="tab active"><Link to='/register'>Sign Up</Link></li>
+      </ul>
+
+      <div className="tab-content">
+        <h1>Sign Up for Free</h1>
+        <form>
+          <div className="top-row">
+            <div className="field-wrap">
+              <label className={labelClass.name}>
+                First Name<span className="req">*</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                value={form.name}
+                required
+                autoComplete="off"
+                onChange={changeHandler}
+              />
+            </div>
+
+            <div className="field-wrap">
+              <label className={labelClass.surname}>
+                Last Name<span className="req">*</span>
+              </label>
+              <input
+                type="text"
+                name="surname"
+                value={form.surname}
+                required
+                autoComplete="off"
+                onChange={changeHandler}
+              />
+            </div>
+          </div>
+
+          <div className="field-wrap">
+            <label className={labelClass.email}>
+              Email Address<span className="req">*</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={form.email}
+              required
+              autoComplete="off"
+              onChange={changeHandler}
+            />
+          </div>
+
+          <div className="field-wrap">
+            <label className={labelClass.password}>
+              Set A Password<span className="req">*</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              value={form.password}
+              required
+              autoComplete="off"
+              onChange={changeHandler}
+            />
+          </div>
+
+          <input type="submit" className="button button-block" value="Get Started"/>
+        </form>
+      </div>
     </div>
   );
 };
