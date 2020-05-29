5057cda800fed9f84156b84bc982b723b6c3f5e7
Navigator12 <mykhailo.rusyn@uzhnu.edu.ua>
2020-05-29 19:38:23 +0300

    Add users to contest (back-end)

--- routes/contest.routes.js
+++ routes/contest.routes.js
@@ -58,7 +58,6 @@ router.get("/index", auth, async (req, res) => {
 });
 
 // /api/contest/:id
-
 router.get("/:id", async (req, res) => {
   try {
     const contestId = req.params.id;
@@ -71,5 +70,46 @@ router.get("/:id", async (req, res) => {
     res.status(500).json({ message: "Something went wrong, try again" });
   }
 });
+
+// /api/contest/:id
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const contestId = req.params.id;
+
+    const teacher = await Teacher.findOne({ _id: new ObjectId(userId) });
+    if (!teacher)
+      return res.status(400).json({ message: "Викладача не знайдено" });
+
+    const contest = await Contest.findOne({ _id: new ObjectId(contestId) });
+    if (!contest)
+      return res.status(400).json({ message: "Контест не знайдено" });
+
+    const { studentIds } = req.body;
+
+    for (const id of studentIds) {
+      let student;
+      try {
+        student = await User.findOne({ _id: new ObjectId(id) });
+      } catch (e) {
+        continue;
+      }
+
+      if (student) {
+        student.contests.push(contest._id);
+        contest.students.push(student._id);
+
+        await student.save();
+      }
+    }
+
+    await contest.save();
+
+    res.json({ message: "Successfully updated" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Something went wrong, try again" });
+  }
+})
 
 module.exports = router;
